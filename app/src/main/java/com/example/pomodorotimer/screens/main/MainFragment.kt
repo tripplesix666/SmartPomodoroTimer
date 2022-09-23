15ff1aeb4fe04d5d10d791e4e5499ee37258c73d
tripplesix666 <nikitazemerov123@gmail.com>
2022-09-23 12:17:55 +0500

    second commit

--- app/src/main/java/com/example/pomodorotimer/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/pomodorotimer/screens/main/MainFragment.kt
@@ -8,12 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.pomodorotimer.databinding.FragmentMainBinding
+import com.example.pomodorotimer.utilits.showToast
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     lateinit var viewModel: MainViewModel
+    private lateinit var circularProgressBar: CircularProgressBar
+    private var millis: Long = 1_500_000
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,15 +32,22 @@ class MainFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         initialization()
-        val circularProgressBar = binding.circularProgressBar
+        circularProgressBar = binding.circularProgressBar
         circularProgressBar.apply {
-            progress = 10f
+            progressMax = millis.toFloat()
         }
     }
 
     private fun initialization() {
         binding.startBtnStart.setOnClickListener {
-            viewModel.startTimer()
+            if (binding.minutesToWorking.text.isNotEmpty()) {
+                val minutes = binding.minutesToWorking.text.toString().toInt()
+                millis = convertMinutesToMillis(minutes)
+            }
+//            showToast(millis.toString())
+            viewModel.startTimer(millis)
+
+
         }
         viewModel.liveDataSeconds.observe(this, Observer {
             binding.startTimeSeconds.text = it
@@ -44,8 +55,15 @@ class MainFragment : Fragment() {
         viewModel.liveDataMinutes.observe(this, Observer {
             binding.startTimeMinutes.text = it
         })
+        viewModel.liveDataMillisUntilFinished.observe(this, Observer {
+            circularProgressBar.progress = 10_000f - it.toFloat()
+        })
     }
 
+    private fun convertMinutesToMillis(testMinutes: Int): Long {
+        val millis = testMinutes * 60_00
+        return millis.toLong()
+    }
 
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/pomodorotimer/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/pomodorotimer/screens/main/MainViewModel.kt
@@ -11,18 +11,22 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     val liveDataSeconds = MutableLiveData<String>()
     val liveDataMinutes = MutableLiveData<String>()
+    val liveDataMillisUntilFinished = MutableLiveData<String>()
 
-    fun startTimer() {
-        object : CountDownTimer(1_500_000, 1000) {
+    fun startTimer(millisUntilFinished: Long) {
+        showToast("$millisUntilFinished")
+        object : CountDownTimer(millisUntilFinished, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 val second = floor((millisUntilFinished.toDouble() / 1000) % 60).toInt()
                 val minutes = floor((millisUntilFinished.toDouble() / (1000 * 60)) % 60).toInt()
                 liveDataSeconds.value = second.toString()
                 liveDataMinutes.value = minutes.toString()
+                liveDataMillisUntilFinished.value = millisUntilFinished.toString()
 
             }
 
             override fun onFinish() {
+
                 showToast("Завершено")
             }
 
